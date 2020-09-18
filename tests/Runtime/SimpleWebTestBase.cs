d4ed516663967fbd5d0e7945bb412935f54567b3
James Frowen <jamesfrowendev@gmail.com>
2020-09-18 15:47:12 +0100

    adding test base file

---
+++ tests/Runtime/SimpleWebTestBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Mirror.SimpleWeb.Tests
+{
+    public class SimpleWebTestBase
+    {
+        List<GameObject> toCleanup = new List<GameObject>();
+
+        [TearDown]
+        public void TearDown()
+        {
+            foreach (GameObject obj in toCleanup)
+            {
+                if (obj != null)
+                {
+                    Transport transport;
+                    if ((transport = obj.GetComponent<Transport>()) != null)
+                    {
+                        transport.Shutdown();
+                    }
+                    GameObject.DestroyImmediate(obj);
+                }
+            }
+        }
+
+        protected SimpleWebTransport CreateRelayTransport()
+        {
+            GameObject go = new GameObject();
+            toCleanup.Add(go);
+
+            SimpleWebTransport transport = go.AddComponent<SimpleWebTransport>();
+            transport.port = 7776;
+            return transport;
+        }
+    }
+}
