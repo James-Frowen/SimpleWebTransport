e5f7928f1c103d20623c11923afab02688086787
James Frowen <jamesfrowendev@gmail.com>
2020-09-20 01:24:45 +0100

    test for disconnect being called when client app exists

--- tests/Runtime/ConnectTest.cs
+++ tests/Runtime/ConnectTest.cs
@@ -6,24 +6,6 @@ using UnityEngine.TestTools;
 
 namespace Mirror.SimpleWeb.Tests
 {
-    [Category("SimpleWebTransport")]
-    public class StartAndStopTest : SimpleWebTestBase
-    {
-        [UnityTest]
-        public IEnumerator ServerCanStartAndStopWithoutErrors()
-        {
-            SimpleWebTransport transport = CreateRelayTransport();
-            transport.ServerStart();
-
-            yield return new WaitForSeconds(0.2f);
-
-            transport.ServerStop();
-
-            yield return new WaitForSeconds(0.2f);
-
-            Assert.Pass("Pass becuase there is no errors");
-        }
-    }
 
     [Category("SimpleWebTransport")]
     public class ConnectAndCloseTest : SimpleWebTestBase
@@ -47,15 +29,15 @@ namespace Mirror.SimpleWeb.Tests
             }
             RunNode.Result result = task.Result;
 
-            Assert.That(result.timedOut, Is.False);
-            Assert.That(result.output, Has.Length.EqualTo(1));
-            Assert.That(result.output[0], Is.EqualTo("Connection opened"));
-            Assert.That(result.error, Has.Length.EqualTo(0));
+            Assert.That(result.timedOut, Is.False, "js should close before timeout");
+            Assert.That(result.output, Has.Length.EqualTo(1), "Should have 1 log");
+            Assert.That(result.output[0], Is.EqualTo("Connection opened"), "Should be connection open log");
+            Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
 
             // wait for message to be processed
             yield return new WaitForSeconds(0.2f);
 
-            Assert.That(onConnectedCalled, Is.EqualTo(1));
+            Assert.That(onConnectedCalled, Is.EqualTo(1), "Connect should be called once");
         }
 
         [UnityTest]
@@ -83,16 +65,54 @@ namespace Mirror.SimpleWeb.Tests
             }
             RunNode.Result result = task.Result;
 
-            Assert.That(result.timedOut, Is.False);
-            Assert.That(result.output, Has.Length.EqualTo(1));
-            Assert.That(result.output[0], Is.EqualTo("Connection opened"));
-            Assert.That(result.error, Has.Length.EqualTo(0));
+            Assert.That(result.timedOut, Is.False, "js should close before timeout");
+            Assert.That(result.output, Has.Length.EqualTo(1), "Should have 1 log");
+            Assert.That(result.output[0], Is.EqualTo("Connection opened"), "Should be connection open log");
+            Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
 
             // wait for message to be processed
             yield return new WaitForSeconds(0.2f);
 
-            Assert.That(onConnectedCalled, Is.EqualTo(1));
-            Assert.That(onDisconnectedCalled, Is.EqualTo(1));
+            Assert.That(onConnectedCalled, Is.EqualTo(1), "Connect should be called once");
+            Assert.That(onDisconnectedCalled, Is.EqualTo(1), "Disconnected should be called once");
+        }
+
+        [UnityTest]
+        public IEnumerator ServerReactsWithClientAppStops()
+        {
+            SimpleWebTransport transport = CreateRelayTransport();
+            transport.ServerStart();
+            int onConnectedCalled = 0;
+            int onDisconnectedCalled = 0;
+            transport.OnServerConnected.AddListener((int connId) =>
+            {
+                onConnectedCalled++;
+                Assert.That(connId, Is.EqualTo(1), "First connection should have id 1");
+            });
+            transport.OnServerDisconnected.AddListener((int connId) =>
+            {
+                onDisconnectedCalled++;
+                Assert.That(connId, Is.EqualTo(1), "First connection should have id 1");
+            });
+
+            // kill js early so it doesn't send close message
+            Task<RunNode.Result> task = RunNode.RunAsync("Connect.js", 1000);
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+            RunNode.Result result = task.Result;
+
+            Assert.That(result.timedOut, Is.True, "Should timeout app stopping before close");
+            Assert.That(result.output, Has.Length.EqualTo(1), "Should have 1 log");
+            Assert.That(result.output[0], Is.EqualTo("Connection opened"), "Should be connection open log");
+            Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
+
+            // wait for message to be processed
+            yield return new WaitForSeconds(0.2f);
+
+            Assert.That(onConnectedCalled, Is.EqualTo(1), "Connect should be called once");
+            Assert.That(onDisconnectedCalled, Is.EqualTo(1), "Disconnected should be called once");
         }
     }
 }
--- tests/node~/Connect.js
+++ tests/node~/Connect.js
@@ -4,11 +4,13 @@ const WebSocket = require("websocket").w3cwebsocket;
 const webSocket = new WebSocket("ws://localhost:7776/");
 webSocket.binaryType = 'arraybuffer';
 
+const closeTimeout = 2000;
+
 // Connection opened
 webSocket.addEventListener('open', function (event) {
     console.log('Connection opened');
     setTimeout(() => {
         webSocket.close(1000, "end");
         process.exit(0);
-    }, 1000);
+    }, closeTimeout);
 });
