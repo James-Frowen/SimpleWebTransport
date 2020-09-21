031bfc2e489b09b6ce4749a616535512de8ce48a
James Frowen <jamesfrowendev@gmail.com>
2020-09-21 16:36:26 +0100

    moving auto close to its own js

--- tests/node~/ConnectAndClose.js
+++ tests/node~/ConnectAndClose.js
@@ -5,13 +5,24 @@ const webSocket = new WebSocket("ws://localhost:7776/");
 webSocket.binaryType = 'arraybuffer';
 
 const closeTimeout = 2000;
+let closed = false;
 
 // Connection opened
 webSocket.addEventListener('open', function (event) {
     console.log('Connection opened');
+    setTimeout(() => {
+        if (!closed) {
+            closed = true;
+            console.log(`Closed after ${closeTimeout}ms`);
+            webSocket.close(1000, `Closed after ${closeTimeout}ms`);
+            setTimeout(() => {
+                process.exit(0);
+            }, 50);
+        }
+    }, closeTimeout);
 });
 
 webSocket.addEventListener('close', function (event) {
-    console.log('Connection closed');
+    // stop process in case close is called
     process.exit(0);
 });
