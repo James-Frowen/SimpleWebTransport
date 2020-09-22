c6752503ce7eceab70416cd74c6de03531d97092
James Frowen <jamesfrowendev@gmail.com>
2020-09-22 15:54:22 +0100

    adding test that opens 100 clients that send pings to server

--- tests/Runtime/Server/ManyClientTest.cs
+++ tests/Runtime/Server/ManyClientTest.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using UnityEngine;
@@ -12,18 +14,22 @@ namespace Mirror.SimpleWeb.Tests.Server
     {
         protected override bool StartServer => true;
 
-        List<Task<RunNode.Result>> clients = new List<Task<RunNode.Result>>();
 
         [UnityTest]
-        public IEnumerator ConnectWithMultipleClients()
+        [TestCase(1, ExpectedResult = null)]
+        [TestCase(10, ExpectedResult = null)]
+        [TestCase(100, ExpectedResult = null)]
+        public IEnumerator ConnectWithMultipleClients(int count)
         {
+            List<Task<RunNode.Result>> clients = new List<Task<RunNode.Result>>();
+
             int connectIndex = 1;
             transport.OnServerConnected.AddListener((connId) =>
             {
                 Assert.That(connId == connectIndex, "Clients should be connected in order with the next index");
                 connectIndex++;
             });
-            const int count = 100;
+
             for (int i = 0; i < count; i++)
             {
                 // connect good client
@@ -52,6 +58,87 @@ namespace Mirror.SimpleWeb.Tests.Server
                 Assert.That(result.output[0], Is.EqualTo("Connection opened"), "Should be connection open log");
                 Assert.That(result.output[1], Is.EqualTo($"Closed after 2000ms"), "Should be connection close log");
                 Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
+            }
+        }
+
+        [UnityTest]
+        [TestCase(1, ExpectedResult = null)]
+        [TestCase(10, ExpectedResult = null)]
+        [TestCase(100, ExpectedResult = null)]
+        public IEnumerator ManyClientsPinging(int count)
+        {
+            List<Task<RunNode.Result>> clients = new List<Task<RunNode.Result>>();
+
+            int connectIndex = 1;
+            transport.OnServerConnected.AddListener((connId) =>
+            {
+                Assert.That(connId == connectIndex, "Clients should be connected in order with the next index");
+                connectIndex++;
+            });
+
+            for (int i = 0; i < count; i++)
+            {
+                // connect good client
+                Task<RunNode.Result> task = RunNode.RunAsync("Ping.js", 30_000);
+                clients.Add(task);
+
+                yield return null;
+            }
+
+            // 4 seconds should be enough time for clients to connect then close themselves
+            yield return new WaitForSeconds(4);
+
+            Assert.That(onConnect, Has.Count.EqualTo(count), "All should be connectted");
+
+
+            // make sure all clients start connected for a while
+            const int seconds = 10;
+            for (float i = 0; i < seconds; i += Time.deltaTime)
+            {
+                Assert.That(onDisconnect, Has.Count.EqualTo(0), "no clients should be disconnected");
+
+                yield return null;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                // 1 indexed
+                transport.ServerDisconnect(i + 1);
+            }
+
+            // wait for all to disconnect
+            yield return new WaitForSeconds(1);
+            Assert.That(onDisconnect, Has.Count.EqualTo(count), "no clients should be disconnected");
+
+            // check all tasks finished with no logs
+            for (int i = 0; i < count; i++)
+            {
+                Task<RunNode.Result> task = clients[0];
+                Assert.That(task.IsCompleted, Is.True, "Take should have been completed");
+
+                RunNode.Result result = task.Result;
+
+                Assert.That(result.timedOut, Is.False, "js should close before timeout");
+                Assert.That(result.output, Has.Length.EqualTo(0), "Should have 2 log");
+                Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
+            }
+
+
+            List<ArraySegment<byte>>[] messageForClients = Enumerable.Repeat(0, count).Select(x => new List<ArraySegment<byte>>()).ToArray();
+            onData.ForEach(x => messageForClients[x.connId - 1 /*from 1 index to 0 indexed*/].Add(x.data));
+            for (int i = 0; i < count; i++)
+            {
+                List<ArraySegment<byte>> messages = messageForClients[i];
+                int expected = seconds - 1;
+                Assert.That(messages, Has.Count.AtLeast(expected), $"Should have atleast {expected} ping messages");
+
+                foreach (ArraySegment<byte> message in messages)
+                {
+                    Assert.That(message.Array[message.Offset + 0], Is.EqualTo(10), "first byte should be 10");
+                    Assert.That(message.Array[message.Offset + 1], Is.EqualTo(11), "second byte should be 11");
+                    Assert.That(message.Array[message.Offset + 2], Is.EqualTo(12), "thrid byte should be 12");
+                    Assert.That(message.Array[message.Offset + 3], Is.EqualTo(13), "fourth byte should be 13");
+                }
             }
         }
     }
---
+++ tests/node~/Ping.js
@@ -0,0 +1,22 @@
+const WebSocket = require("websocket").w3cwebsocket;
+
+// Create webSocket connection.
+const webSocket = new WebSocket("ws://localhost:7776/");
+webSocket.binaryType = 'arraybuffer';
+
+const pingInterval = 1000;
+// Connection opened
+webSocket.addEventListener('open', function (event) {
+    setInterval(() => {
+        var buffer = new ArrayBuffer(4);
+        var view = new Uint8Array(buffer);
+        for (let i = 0; i < view.length; i++) {
+            view[i] = i + 10;
+        }
+        webSocket.send(buffer);
+    }, pingInterval);
+});
+
+webSocket.addEventListener('close', function (event) {
+    process.exit(0);
+});
