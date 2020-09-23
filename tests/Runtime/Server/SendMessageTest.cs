c7ac91b3354644ec7e8264b774b81626731f6d9b
James Frowen <jamesfrowendev@gmail.com>
2020-09-23 02:05:05 +0100

    adding test for send many
    note: node seems to cut off logs if lines are too long

--- tests/Runtime/Server/SendMessageTest.cs
+++ tests/Runtime/Server/SendMessageTest.cs
@@ -38,5 +38,43 @@ namespace Mirror.SimpleWeb.Tests.Server
             Assert.That(result.output[0], Is.EqualTo(expected), "Should have message log");
             Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
         }
+
+        [UnityTest]
+        public IEnumerator SendMany()
+        {
+            Task<RunNode.Result> task = RunNode.RunAsync("ReceiveManyMessages.js", 5_000);
+
+            yield return WaitForConnect;
+            const int messageCount = 100;
+
+            for (int i = 0; i < messageCount; i++)
+            {
+                using (PooledNetworkWriter writer = NetworkWriterPool.GetWriter())
+                {
+                    writer.WriteByte((byte)i);
+                    writer.WriteInt32(100);
+
+                    ArraySegment<byte> segment = writer.ToArraySegment();
+
+                    transport.ServerSend(new List<int> { 1 }, Channels.DefaultReliable, segment);
+                }
+            }
+
+            yield return new WaitForSeconds(1);
+            transport.ServerDisconnect(1);
+
+            yield return new WaitUntil(() => task.IsCompleted);
+
+            RunNode.Result result = task.Result;
+
+            string expected = "length: 5 msg: {0:X2} 64 00 00 00";
+            Assert.That(result.timedOut, Is.False, "js should close before timeout");
+            Assert.That(result.output, Has.Length.EqualTo(messageCount), $"Should have {messageCount} logs");
+            for (int i = 0; i < messageCount; i++)
+            {
+                Assert.That(result.output[i].ToLower(), Is.EqualTo(string.Format(expected, i).ToLower()), "Should have message log");
+            }
+            Assert.That(result.error, Has.Length.EqualTo(0), "Should have no errors");
+        }
     }
 }
---
+++ tests/node~/ReceiveManyMessages.js
@@ -0,0 +1,45 @@
+const WebSocket = require("websocket").w3cwebsocket;
+
+// Create webSocket connection.
+const webSocket = new WebSocket("ws://localhost:7776/");
+webSocket.binaryType = 'arraybuffer';
+
+// use ping to keep connection alive
+const pingInterval = 1000;
+// Connection opened
+webSocket.addEventListener('open', function (event) {
+    setInterval(() => {
+        var buffer = new ArrayBuffer(4);
+        var view = new Uint8Array(buffer);
+        for (let i = 0; i < view.length; i++) {
+            view[i] = i + 10;
+        }
+        webSocket.send(buffer);
+    }, pingInterval);
+});
+
+// let msgCount = 0;
+// Connection opened
+webSocket.addEventListener('message', function (event) {
+    var buffer = event.data;
+    if (buffer instanceof ArrayBuffer) {
+        // msgCount++;
+        // console.log(msgCount);
+        console.log(`length: ${buffer.byteLength} msg: ${buf2hex(buffer)}`);
+    }
+    else {
+        console.error("Message not array buffer");
+    }
+});
+
+webSocket.addEventListener('error', function (event) {
+    console.error('Socket Error', event.data);
+})
+
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join(' ');
+}
+
+webSocket.addEventListener('close', function (event) {
+    process.exit(0);
+});
