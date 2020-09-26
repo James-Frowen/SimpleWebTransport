19424297103dd031ced7cf6ac62bc3c33933baad
James Frowen <jamesfrowendev@gmail.com>
2020-09-26 01:58:09 +0100

    Adding debug scripts

---
+++ DebugScripts/.DebugClient.js
@@ -0,0 +1,44 @@
+// copy paste this to console in chrome new tab
+(() => {
+    let webSocket = new WebSocket("wss://localhost:7776/");
+    webSocket.binaryType = 'arraybuffer';
+
+    const pingInterval = 1000;
+    let intervalHandle;
+
+    webSocket.addEventListener('error', function (event) {
+        console.error('Socket Error', event);
+    });
+
+    webSocket.addEventListener('open', function (event) {
+        console.log('Open event');
+        intervalHandle = setInterval(() => {
+            var buffer = new ArrayBuffer(4);
+            var view = new Uint8Array(buffer);
+            for (let i = 0; i < view.length; i++) {
+                view[i] = i + 10;
+            }
+            console.log('Send Ping');
+            webSocket.send(buffer);
+        }, pingInterval);
+    });
+
+    webSocket.addEventListener('close', function (event) {
+        console.log('Close event');
+        clearInterval(intervalHandle);
+    });
+
+    webSocket.addEventListener('message', function (event) {
+        var buffer = event.data;
+        if (buffer instanceof ArrayBuffer) {
+            console.log(`length: ${buffer.byteLength} msg: ${buf2hex(buffer)}`);
+        }
+        else {
+            console.error("Message not array buffer");
+        }
+    });
+
+    function buf2hex(buffer) { // buffer is an ArrayBuffer
+        return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('-');
+    }
+})();
---
+++ DebugScripts/DebugServer.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror.SimpleWeb
+{
+    [RequireComponent(typeof(SimpleWebTransport))]
+    public class DebugServer : MonoBehaviour
+    {
+        public Vector2 guiPosition;
+        private SimpleWebTransport transport;
+
+        private void Start()
+        {
+            transport = GetComponent<SimpleWebTransport>();
+            transport.OnServerConnected.AddListener(onConnect);
+            transport.OnServerDataReceived.AddListener(onData);
+            transport.OnServerDisconnected.AddListener(onDisconnect);
+            transport.OnServerError.AddListener(onError);
+            transport.ServerStart();
+        }
+
+        private void onConnect(int connId)
+        {
+            Debug.Log($"onConnect:{connId}");
+        }
+
+        private void onData(int connId, ArraySegment<byte> data, int channel)
+        {
+            string str = BitConverter.ToString(data.Array, data.Offset, data.Count);
+            Debug.Log($"onData:{connId} data:{str}");
+        }
+
+        private void onDisconnect(int connId)
+        {
+            Debug.Log($"onDisconnect:{connId}");
+        }
+
+        private void onError(int connId, Exception e)
+        {
+            Debug.Log($"onError:{connId}");
+        }
+
+
+        public void OnGUI()
+        {
+            using (new GUILayout.AreaScope(new Rect(guiPosition, new Vector2(500, 500))))
+            {
+                if (GUILayout.Button("Send Message"))
+                {
+                    transport.ServerSend(new List<int>() { 1 }, Channels.DefaultReliable, new ArraySegment<byte>(new byte[] { 1, 2, 4, 8 }));
+                }
+            }
+        }
+    }
+}
