cc6e51361b54c5d81763bb8ebf44ed7355426421
James Frowen <jamesfrowendev@gmail.com>
2020-10-10 02:04:44 +0100

    feat: Allowing multiple instances of javascript client (#14)
    * refactoring js client to allow for multiple instances
    
    * tests for opening multiple instances of the client

---
+++ tests/Runtime/Client/ClientTestsMultiple.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Mirror.SimpleWeb.Tests.Client
+{
+    [Category("SimpleWebTransport")]
+    public class ClientTestsMultiple : SimpleWebClientTestBase
+    {
+        protected override bool StartServer => true;
+
+        [UnityTest]
+        public IEnumerator CanCreateMultipleClientInstances()
+        {
+            int count = 10;
+            SimpleWebTransport[] transports = new SimpleWebTransport[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                transports[i] = CreateTransport();
+
+                transports[i].ClientConnect("localhost");
+                yield return new WaitForSeconds(0.2f);
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+
+            Assert.That(server_onConnect, Has.Count.EqualTo(count), $"Connect should have been calle {count} times");
+
+            for (int i = 0; i < count; i++)
+            {
+                transports[i].ClientDisconnect();
+                yield return new WaitForSeconds(0.2f);
+            }
+
+            // wait for disconnect
+            yield return new WaitForSeconds(1);
+
+            Assert.That(server_onDisconnect, Has.Count.EqualTo(count), $"disconnect should have been calle {count} times");
+        }
+    }
+}
