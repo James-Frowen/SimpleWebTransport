fdaf3caf541915ac8d4b20f192246bd658a57bbb
James Frowen <jamesfrowendev@gmail.com>
2020-10-16 20:44:13 +0100

    Update pfxTestServer.js

--- DebugScripts/node~/pfxTestServer.js
+++ DebugScripts/node~/pfxTestServer.js
@@ -4,11 +4,14 @@ const https = require('https');
 const fs = require('fs');
 const port = 8000;
 
+console.log("Listening on " + port);
+
 const options = {
     pfx: fs.readFileSync('testCert.pfx')
 };
 
 https.createServer(options, (req, res) => {
+    console.log("Request from " + (req.socket ? req.socket.remoteAddress : "NULL"));
     res.writeHead(200);
     res.end('hello world\n');
 }).listen(port);
