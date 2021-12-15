c9cf696bebc8409ec8893942c93252cf5c1f8e0d
James Frowen <jamesfrowendev@gmail.com>
2021-12-15 17:58:55 +0000

    feat: adding ProcessMessageQueue that doesn't require a monobehaviour

--- source/Server/SimpleWebServer.cs
+++ source/Server/SimpleWebServer.cs
@@ -70,12 +70,25 @@ namespace JamesFrowen.SimpleWeb
             return server.GetClientAddress(connectionId);
         }
 
+        /// <summary>
+        /// Processes all new messages
+        /// </summary>
+        public void ProcessMessageQueue()
+        {
+            ProcessMessageQueue(null);
+        }
+
+        /// <summary>
+        /// Processes all messages while <paramref name="behaviour"/> is enabled
+        /// </summary>
+        /// <param name="behaviour"></param>
         public void ProcessMessageQueue(MonoBehaviour behaviour)
         {
             int processedCount = 0;
+            bool skipEnabled = behaviour == null;
             // check enabled every time incase behaviour was disabled after data
             while (
-                behaviour.enabled &&
+                (skipEnabled || behaviour.enabled) &&
                 processedCount < maxMessagesPerTick &&
                 // Dequeue last
                 server.receiveQueue.TryDequeue(out Message next)
