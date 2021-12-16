a82180fd31a8d0a0b58bf6092a161eb2cd758711
James Frowen <jamesfrowendev@gmail.com>
2021-12-16 16:08:18 +0000

    CI: fixing release config

--- source/AssemblyInfo.cs
+++ source/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyVersion("0.9.0")]
+
 [assembly: InternalsVisibleTo("SimpleWebTransport.Tests.Runtime")]
 [assembly: InternalsVisibleTo("SimpleWebTransport.Tests.Editor")]
